cdba17f91a065ef66ac1d79dd0adc5395e1f9bfe
AZIZ BENHIDA <harry530.aziz@gmail.com>
2020-11-15 20:25:27 +0100

    Add scrupt.js

---
+++ script.js
@@ -0,0 +1,56 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+        return userInput;
+    } else {
+        console.log('Error!');
+    }
+}
+function getComputerChoice() {
+    let m = Math.floor(Math.random() * 3);
+    switch (m) {
+        case 0:
+            return 'rock';
+            break;
+        case 1:
+            return 'paper';
+            break;
+        case 2:
+            return 'scissors';
+            break;
+    }
+}
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return 'The game was a tie!';
+    } else if (userChoice == 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    } else if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    } else if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    } else if (userChoice === 'bomb') {
+        return 'You won!';
+    }
+}
+function playGame() {
+    const userChoice = getUserChoice('rock');
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw: ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+
+}
+playGame();
